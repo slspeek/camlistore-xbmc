05d442a5df3afe01ce7d0e22f6c12a89ced0efe7
Steven L. Speek <slspeek@gmail.com>
2014-06-14 00:02:58 +0200

    Graphics

---
+++ makezips.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+zip -r script.module.camlistore.zip script.module.camlistore/
+zip -r  plugin.audio.camlistore.zip plugin.audio.camlistore/
+zip -r plugin.video.camlistore.zip plugin.video.camlistore/
+zip -r plugin.image.camlistore.zip plugin.image.camlistore/
