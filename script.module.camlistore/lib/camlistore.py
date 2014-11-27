e74d49c5af429493228bd17dbed3ff8d6647d104
Steven L. Speek <slspeek@gmail.com>
2014-11-27 21:21:36 +0100

    https added. Whitespace --> spaces.

--- script.module.camlistore/lib/camlistore.py
+++ script.module.camlistore/lib/camlistore.py
@@ -7,99 +7,107 @@
 import requests
 from xbmcswift2 import xbmcaddon
 
-def xbmcConfig(name):
-	addon = xbmcaddon.Addon(name)
-	return Config(
-			addon.getSetting('address'),
-			addon.getSetting('username'),
-			addon.getSetting('password'))
 
 class Config:
 
-	def __init__(self, address, username, password):
-		self._address = address
-		self._username = username
-		self._password = password
+    def __init__(self, https, address, username, password):
+        self._https = https
+        self._address = address
+        self._username = username
+        self._password = password
 
-	@property
-	def address(self):
-		return self._address
+    @property
+    def https(self):
+        """docstring for https"""
+        return self._https
 
-	@property
-	def username(self):
-		return self._username
+    @property
+    def address(self):
+        return self._address
 
-	@property
-	def password(self):
-		return self._password
+    @property
+    def username(self):
+        return self._username
 
+    @property
+    def password(self):
+        return self._password
+
+    def protocol(self):
+        return "https://" if self.https else  "http://"
+
+def xbmcConfig(name):
+    addon = xbmcaddon.Addon(name)
+    return Config(
+            addon.getSetting('https'),
+            addon.getSetting('address'),
+            addon.getSetting('username'),
+            addon.getSetting('password'))
 
 class Search:
 
-	def __init__(self, config):
-		self.config = config
-		self.conf = self._conf_discovery()
+    def __init__(self, config):
+        self.config = config
+        self.conf = self._conf_discovery()
 
-	def _conf_discovery(self):
-		""" Perform a discovery to gather server configuration. """
-		r = requests.get('http://' + self.config.address,
-				auth=self.auth(),
-				headers={'Accept': 'text/x-camli-configuration'})
-		r.raise_for_status()
-		return r.json()
+    def _conf_discovery(self):
+        """ Perform a discovery to gather server configuration. """
+        r = requests.get(self.config.protocol() + self.config.address,
+                auth=self.auth(),
+                headers={'Accept': 'text/x-camli-configuration'},
+                verify=False)
+        r.raise_for_status()
+        return r.json()
 
 
-	def auth(self):
-		return (self.config.username, self.config.password)
+    def auth(self):
+        return (self.config.username, self.config.password)
 
-	def _url(self,path):
-		'''Returns a full url for the given path'''            
-		return 'http://' + self.config.address +  path
+    def _url(self,path):
+        '''Returns a full url for the given path'''            
+        return self.config.protocol() + self.config.address +  path
 
-	def _get(self, url, data=None):
-		resp = requests.post(url, data=data,auth=self.auth())
-		#print "Resp:", resp.text
-		return resp.json()
+    def _get(self, url, data=None):
+        resp = requests.post(url, data=data,auth=self.auth(), verify=False)
+        resp.raise_for_status()
+        return resp.json()
 
-	def query(self, expression):
-		''' Returns a list of playable permanodes '''
-		data = '{"expression": "%s", "describe":{ "depth": 2}}' % expression
-		searchRoot = self.conf['searchRoot']
-		print 'searchRoot', searchRoot
-		url = self._url(searchRoot + 'camli/search/query')
-		print url
-		j = self._get(url, data=data)
-		if not j.has_key("blobs"):
-			return []
+    def query(self, expression):
+        ''' Returns a list of playable permanodes '''
+        data = '{"expression": "%s", "describe":{ "depth": 2}}' % expression
+        searchRoot = self.conf['searchRoot']
+        url = self._url(searchRoot + 'camli/search/query')
+        j = self._get(url, data=data)
+        if not j.has_key("blobs"):
+            return []
 
-		blobs = j["blobs"]
-		meta = j["description"]["meta"]
-		items = []
-		if not blobs or not meta:
-			return []
+        blobs = j["blobs"]
+        meta = j["description"]["meta"]
+        items = []
+        if not blobs or not meta:
+            return []
 
-		print blobs, meta
-		for k in blobs:
-			k = k["blob"]
-			attrs = meta[k]["permanode"]["attr"]
-			filename = k
-			fileref = None
-			title = None
-			if attrs.has_key("camliContent"):
-				fileref = attrs["camliContent"][0]
-				filename = meta[fileref]["file"]["fileName"]
-			if attrs.has_key('title'):
-				title = attrs["title"][0]
-			if title:
-				label = title
-			else:
-				label = filename
-			if fileref:
-				items.append({
-					'label': label,
-					'path': 'http://' + self.config.username + ':'+ self.config.password + '@' + self.config.address + '/ui/download/' + fileref,
-					'is_playable': True,
-					#'info_type': 'pictures',
-					})
-		return items
+        for k in blobs:
+            k = k["blob"]
+            attrs = meta[k]["permanode"]["attr"]
+            filename = k
+            fileref = None
+            title = None
+            if attrs.has_key("camliContent"):
+                fileref = attrs["camliContent"][0]
+                filename = meta[fileref]["file"]["fileName"]
+            if attrs.has_key('title'):
+                title = attrs["title"][0]
+            if title:
+                label = title
+            else:
+                label = filename
+            if fileref:
+                items.append({
+                    'label': label,
+                    'path': self.config.protocol() + self.config.username + ':'+ self.config.password + '@' + self.config.address + '/ui/download/' + fileref,
+                    'is_playable': True,
+                    #'info_type': 'pictures',
+                    })
+        return items
 
--- script.module.camlistore/lib/camlistore_test.py
+++ script.module.camlistore/lib/camlistore_test.py
@@ -6,28 +6,35 @@ import subprocess
 
 from lib import camlistore 
 
-config = camlistore.Config('localhost:3179', 'camli', 'pass3179')
 
-class TestNoseCases(unittest.TestCase):
+class QueryEmptyServer(unittest.TestCase):
 
-	@classmethod
-	def setUpClass(cls):
-		cwd = os.getcwd()
-		os.chdir(os.environ['CAMLI_SRC'])
-		cls.devcam_server = subprocess.Popen(['bin/devcam', 'server'])
-		os.chdir(cwd)
-		time.sleep(3)
+    @classmethod
+    def setUpClass(cls):
+        cls.config = camlistore.Config(False, 'localhost:3179', 'camli', 'pass3179')
+        cwd = os.getcwd()
+        os.chdir(os.environ['CAMLI_SRC'])
+        cls.devcam_server = subprocess.Popen(['bin/devcam', 'server'])
+        os.chdir(cwd)
+        time.sleep(3)
 
-	@classmethod
-	def tearDownClass(cls):
-		cls.devcam_server.kill()
-		
-	def test_query(self):
-		scraper = camlistore.Search(config)
-		resp  = scraper.query("-is:image")
-		import json
-		logging.info(json.dumps(resp, indent=4))
-		self.assertIsNotNone(resp)
-		logging.info("HELLO")
-		self.fail()
-		
+    @classmethod
+    def tearDownClass(cls):
+        cls.devcam_server.kill()
+
+    def test_query(self):
+        scraper = camlistore.Search(self.config)
+        resp  = scraper.query("-is:image")
+        self.assertEqual(len(resp), 0)
+
+class Secure(QueryEmptyServer):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.config = camlistore.Config(True, 'localhost:3179', 'camli', 'pass3179')
+        cwd = os.getcwd()
+        os.chdir(os.environ['CAMLI_SRC'])
+        cls.devcam_server = subprocess.Popen(['bin/devcam', 'server', "-tls"])
+        os.chdir(cwd)
+        time.sleep(3)
+
